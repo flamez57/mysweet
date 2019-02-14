8532ddcb27b7338c9b1cc803d629f1a9062ba918
flamez57 <yangxiang@shunliandongli.com>
2019-02-14 13:38:35 +0800

    开启报错

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+//开启报错显示
+ini_set("display_errors", "On");
+error_reporting(E_ALL);
+
 define("ROOT_PATH", dirname(__FILE__).DIRECTORY_SEPARATOR);
 define("HTTP_HOST", $_SERVER['HTTP_HOST'].'/');
 define("ENGINE_PATH", ROOT_PATH.'hl/');
