c5d222dfc9ee96619ded718dd878c5de4b3ec51d
flamez57 <yangxiang@shunliandongli.com>
2019-04-09 17:33:38 +0800

    获取body元素

--- hl/library/Tools/Html2Array/HLHtmlToArray.php
+++ hl/library/Tools/Html2Array/HLHtmlToArray.php
@@ -2,6 +2,8 @@
 
 namespace hl\library\Tools\Html2Array;
 
+use DOMDocument;
+
 /**
 ** @ClassName: HLHtmlToArray
 ** @Description: HTML数据提取
@@ -11,5 +13,40 @@ namespace hl\library\Tools\Html2Array;
 */
 class HLHtmlToArray
 {
+    /*
+    ** 页面body体
+    */
+    private $body;
 
+    /*
+    ** 错误信息
+    */
+    private $errorMsg;
+
+    /*
+    ** @param $html string HTML页面代码
+    */
+    public function __construct($html)
+    {
+        //如果没有文档类型声明就加上
+        if (false === mb_strpos(mb_strtolower(mb_substr($html, 0, 80)), '<!doctype') ) {
+            $html = "<!DOCTYPE html><html><head><meta charset='UTF-8' /></head><body>{$html}</body></html>";
+        }
+
+        //实例化dom类
+        $dom = new DOMDocument('1.0', 'UTF-8');
+
+        //函数禁用标准的 libxml 错误，并启用用户错误处理。
+        @libxml_use_internal_errors(true);
+        $dom->loadHTML($html);
+        //函数清空 libxml 错误缓冲。
+        @libxml_clear_errors();
+
+        //根据标签获取元素
+        $body = $dom->getElementsByTagName('body');
+        if (0 == $body->length) {
+            $this->errorMsg .= "body is null";
+        }
+        $this->body = $body[0];
+    }
 }
