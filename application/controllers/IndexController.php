ff765856d91e52a37261d42daf391bc7b5a67f11
flamez57 <yangxiang@shunliandongli.com>
2019-04-10 11:28:48 +0800

    html2array

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -17,6 +17,36 @@ class IndexController extends HLController
     */
     public function indexAction()
     {
+        $html = <<<HTML
+<html>
+   <head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+
+     <title>PHP: Hypertext Preprocessor</title>
+
+ <link rel="shortcut icon" href="http://php.net/favicon.ico">
+ <link rel="search" type="application/opensearchdescription+xml" href="http://php.net/phpnetimprovedsearch.src" title="Add PHP.net search">
+ <link rel="alternate" type="application/atom+xml" href="http://php.net/releases/feed.php" title="PHP Release feed">
+ <link rel="alternate" type="application/atom+xml" href="http://php.net/feed.atom" title="PHP: Hypertext Preprocessor">
+
+ <link rel="canonical" href="http://php.net/index.php">
+ <link rel="shorturl" href="http://php.net/index">
+ <link rel="alternate" href="http://php.net/index" hreflang="x-default">
+   </head>
+   <body>       
+    <ul class="nav">
+      <li class=""><a href="/downloads">Downloads</a></li>
+      <li class=""><a href="/docs.php">Documentation</a></li>
+      <li class=""><a href="/get-involved" >Get Involved</a></li>
+      <li class=""><a href="/support">Help</a></li>
+    </ul>
+   </body>
+</html>
+HTML;
+        $hta = new \hl\library\Tools\Html2Array\HLHtmlToArray($html);
+        \hl\library\Functions\Helper::dump($hta->getDom());
+        \hl\library\Functions\Helper::dump($hta->getArray());
         $str = exampleServices::getInstance()->todo();
         //传递值到模板
         \hl\HLView::param('out', $str);
--- hl/library/Tools/Html2Array/HLHtmlToArray.php
+++ hl/library/Tools/Html2Array/HLHtmlToArray.php
@@ -19,6 +19,11 @@ class HLHtmlToArray
     private $body;
 
     /*
+    ** DOM对象
+    */
+    private $dom;
+
+    /*
     ** 错误信息
     */
     private $errorMsg;
@@ -28,10 +33,7 @@ class HLHtmlToArray
     */
     public function __construct($html)
     {
-        //如果没有文档类型声明就加上
-        if (false === mb_strpos(mb_strtolower(mb_substr($html, 0, 80)), '<!doctype') ) {
-            $html = "<!DOCTYPE html><html><head><meta charset='UTF-8' /></head><body>{$html}</body></html>";
-        }
+        $html = $this->prepareForDOM($html);
 
         //实例化dom类
         $dom = new DOMDocument('1.0', 'UTF-8');
@@ -42,11 +44,132 @@ class HLHtmlToArray
         //函数清空 libxml 错误缓冲。
         @libxml_clear_errors();
 
+        $this->dom = $dom;
+
         //根据标签获取元素
         $body = $dom->getElementsByTagName('body');
         if (0 == $body->length) {
             $this->errorMsg .= "body is null";
         }
         $this->body = $body[0];
+    }
+
+    /**
+    ** 整理HTML代码
+    ** @param $html string HTML代码
+    ** @return string
+    */
+    private function prepareForDOM($html)
+    {
+        //获取当前字符串的编码
+        $encoding = mb_detect_encoding($html, ["ASCII",'UTF-8',"GB2312","GBK",'BIG5']);
+        if ($encoding != 'UTF-8') {
+            $html = iconv($encoding, 'UTF-8//TRANSLIT', $html);
+        }
+
+        //如果没有文档类型声明就加上
+        if (false === mb_strpos(mb_strtolower(mb_substr($html, 0, 80)), '<!doctype') ) {
+            $html = "<!DOCTYPE html><html><head><meta charset='UTF-8' /></head><body>{$html}</body></html>";
+        }
+
+        //去除一些仅用于呈现的重要属性，如脚本、样式和其他标记。
+        $html = preg_replace('/<(script|style|noscript)\b[^>]*>.*?<\/\1\b[^>]*>/is', '', $html);
+        //整理HTML代码
+        if (class_exists('\tidy')) {
+            $tidy = new \tidy();
+            $config = array(
+                'drop-font-tags' => true,
+                'drop-proprietary-attributes' => true,
+                'hide-comments' => true,
+                'indent' => true,
+                'logical-emphasis' => true,
+                'numeric-entities' => true,
+                'output-xhtml' => true,
+                'wrap' => 0
+            );
+            $tidy->parseString($html, $config, 'utf8');
+            $tidy->cleanRepair();
+            $html = $tidy->value;
+        }
+        //去除meta head
+        $html = preg_replace('#<meta[^>]+>#isu', '', $html);
+        $html = preg_replace(
+            '#<head\b[^>]*>#isu',
+            "<head>\r\n<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />",
+            $html
+        );
+        return $html;
+    }
+
+    /**
+    ** 获取 SimpleXMLElement 对象
+    ** @return SimpleXMLElement|false
+    */
+    public function getXml()
+    {
+        return simplexml_import_dom($this->dom);
+    }
+
+    /**
+    ** Get DOMDocument
+    ** @return \DOMDocument
+    */
+    public function getDom()
+    {
+        return $this->dom;
+    }
+
+    /**
+    ** Get array
+    ** @return array
+    */
+    public function getArray()
+    {
+        return $this->xml2array($this->getXml());
+    }
+
+    /**
+    ** SimpleXMLElement to array
+    ** @param $xml SimpleXMLElement
+    ** @return array
+    */
+    public function xml2array($xml)
+    {
+        $fils = 0;
+        $tab = false;
+        $array = [];
+        foreach ($xml->children() as $key => $value) {
+            $child = $this->xml2array($value);
+            foreach ($value->attributes() as $ak => $av) {
+                $tmp = (array) $av;
+                $child[$ak] = $tmp;
+            }
+            /**
+             * @var 返回直接位于此元素中的文本内容。不返回该元素子元素内部的文本内容。
+             */
+            $child['text'] = $value->__toString();
+            if (strlen(trim($child['text'])) === 0) {
+                unset($child['text']);
+            }
+
+            if ($tab == false && in_array($key, array_keys($array))) {
+                //如果这个元素已经在数组中，我们将创建一个索引数组
+                $tmp = $array[$key];
+                $array[$key] = NULL;
+                $array[$key][] = $tmp;
+                $array[$key][] = $child;
+                $tab = true;
+            } elseif ($tab == true) {
+                $array[$key][] = $child;
+            } else {
+                $array[$key] = $child;
+            }
+            $fils++;
+        }
+
+        if ($fils == 0) {
+            return (array) $xml;
+        }
+        return $array;
     }
 }
