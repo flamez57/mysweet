271f4d0e7c29c5d77439256ec306a9d4a9812627
flamez <1050355217@qq.com>
2019-05-13 21:39:54 +0800

    套模板

---
+++ pc_shop/controllers/GoodsController.php
@@ -0,0 +1,29 @@
+<?php
+namespace pc_shop\controllers;
+/**
+** @ClassName: IndexController
+** @Description: 商品控制器
+** @author flamez57 <flamez57@mysweet95.com>
+** @date 2018年3月26日 晚上21:49
+** @version V1.0
+*/
+use hl\HLController;
+
+class GoodsController extends HLController
+{
+    /*
+    ** 默认首页
+    */
+    public function listAction()
+    {
+
+    }
+
+    /*
+    ** 详情
+    */
+    public function detailAction()
+    {
+
+    }
+}
