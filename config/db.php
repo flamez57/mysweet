c0d48a39f47d188208d5e98ff6a5fd4c521d5a87
flamez57 <flamez57@mysweet95.com>
2020-09-05 15:19:20 +0800

    排除配置文件

--- config/db.php
+++ config/db.php
@@ -1,27 +1,19 @@
 <?php
 
 return [
-//    'default' => [
-//        'host' => 'localhost',
-//        'dbname' => 'mysweet',
-//        'username' => 'root',
-//        'password' => 'vagrant',
-//        'charset' => 'utf8',
-//        'port' => '3306'
-//    ],
-//    'dev' => [
-//        'host' => '127.0.0.1',
-//        'dbname' => 'sfydb_6392576',
-//        'username' => 'sfydb_6392576',
-//        'password' => 'XiangXiangXiang95',
-//        'charset' => 'utf8',
-//        'port' => '3306'
-//    ],
     'default' => [
-        'host' => '111.230.125.58',
-        'dbname' => 'srm',//'shunlian',
-        'username' => 'dev.yangxiang',
-        'password' => 'A#45gBRHd%$4I2Ih',
+        'host' => 'localhost',
+        'dbname' => 'mysweet',
+        'username' => 'root',
+        'password' => 'vagrant',
+        'charset' => 'utf8',
+        'port' => '3306'
+    ],
+    'dev' => [
+        'host' => '127.0.0.1',
+        'dbname' => 'sfydb_6392576',
+        'username' => 'sfydb_6392576',
+        'password' => 'XiangXiangXiang95',
         'charset' => 'utf8',
         'port' => '3306'
     ],
