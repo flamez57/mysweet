acfe982aa2f63b0c0ae3903dc29f8ab6386d02ff
flamez57 <flamez57@mysweet95.com>
2020-12-01 17:30:13 +0800

    33

--- myblog/src/components/HelloWorld.vue
+++ myblog/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>必要的联系</h2>
     <ul>
       <li>
         <a
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '欢迎'
     }
   }
 }
