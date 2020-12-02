09d492aba2f469f22972acc625274fc756749ef7
flamez57 <flamez57@mysweet95.com>
2020-12-02 11:50:49 +0800

    路由页面

---
+++ myblog/src/components/Record.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>必要的联系</h2>
+    <ul>
+      <li>
+        <router-link to="/">首页</router-link>
+      </li>
+      <li>
+        <router-link to="/Record">档案</router-link>
+      </li>
+      <li>
+        <router-link to="/About">关于我</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Record',
+  data () {
+    return {
+      msg: 'record'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- myblog/src/router/index.js
+++ myblog/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Record from '@/components/Record'
+import About from '@/components/About'
 
 Vue.use(Router)
 
@@ -8,8 +10,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },{
+      path: '/Record',
+      name: 'Record',
+      component: Record
+    },{
+      path: '/About',
+      name: 'About',
+      component: About
     }
   ]
 })
