13620e53771467cbbef7a9d74a15062af1b431b4
flamez57 <flamez57@mysweet95.com>
2021-02-06 16:04:31 +0800

    接口数据请求

--- myblog/src/api/article.js
+++ myblog/src/api/article.js
@@ -9,17 +9,17 @@ import qs from 'qs' // 根据需求是否导入qs模块
 const article = {
   // 新闻列表
   articleList () {
-    return axios.get(Apis.memberInfo)
+    return axios.get(Apis.fmemberInfo)
   },
   // 新闻详情,演示
   articleDetail (id, params) {
-    return axios.get(Apis.memberInfo, {
+    return axios.get(Apis.fmemberInfo, {
       params: params
     })
   },
   // post提交
   login (params) {
-    return axios.post(Apis.memberInfo, qs.stringify(params))
+    return axios.post(Apis.fmemberInfo, qs.stringify(params))
   }
   // 其他接口…………
 }
--- myblog/src/config/apis.js
+++ myblog/src/config/apis.js
@@ -8,8 +8,54 @@ let frontApi = baseUrl + 'm=blogapi&c=index&a='
 let manageApi = baseUrl + 'm=blogapi&c=manage&a=' // 管理端
 
 let Apis = {
-  // 用户信息
-  memberInfo: frontApi + 'memberInfo',
-  memberName: manageApi + 'aa'
+  // 分类 get code
+  fcateList: frontApi + 'cateList',
+  // 标签 get code
+  ftagList: frontApi + 'tagList',
+  // 文章列表 get code page page_size tag_id cate_id keyword
+  farticleList: frontApi + 'articleList',
+  // 用户信息 get code
+  fmemberInfo: frontApi + 'memberInfo',
+  // 文章详情 get id
+  farticleDetail: frontApi + 'articleDetail',
+  // 发表看法 post article_id content nickname
+  faddComment: frontApi + 'addComment',
+  // 回复 post id content
+  freply: frontApi + 'reply',
+  // 档案 get code
+  fdiary: frontApi + 'diary',
+  // 登陆 post code pwd
+  flogin: frontApi + 'login',
+  // --==管理端接口==--
+  // 个人信息详情编辑用 get
+  memberInfoForEdit: manageApi + 'memberInfoForEdit',
+  // 个人信息编辑保存 post nickname avatar motto home_page github qq email content
+  saveMemberInfo: manageApi + 'saveMemberInfo',
+  // 修改密码保存 post pwd pwd_new pwd_new2
+  setPwd: manageApi + 'setPwd',
+  // 文章列表 get keyword page page_size type
+  articleList: manageApi + 'articleList',
+  // 文章详情 get id
+  detail: manageApi + 'detail',
+  // 文章保存 post id cate_id title content status tags
+  save: manageApi + 'save',
+  // 删除文章 post id
+  delArticle: manageApi + 'delArticle',
+  // 发布退回文章 post id status
+  updateStatus: manageApi + 'updateStatus',
+  // 分类下拉 get
+  cateSelect: manageApi + 'cateSelect',
+  // 分类列表 get page page_size
+  cateList: manageApi + 'cateList',
+  // 分类详情 get id
+  cateDetail: manageApi + 'cateDetail',
+  // 分类保存 post name sort status
+  saveCate: manageApi + 'saveCate',
+  // 日记列表 get page page_size year mon day
+  diaryList: manageApi + 'diaryList',
+  // 日记保存 post content
+  addDiary: manageApi + 'addDiary',
+  // 退出 get
+  loginOut: manageApi + 'loginOut'
 }
 export default Apis
