5efb195d7fa299449a99efbf8efd61e92b9c79d4
flamez57 <flamez57@mysweet95.com>
2021-04-06 16:15:21 +0800

    任务中心service

--- config/console.php
+++ config/console.php
@@ -2,11 +2,13 @@
 $common = require(__DIR__ . '/common.php');
 $db = require(__DIR__ . '/db.php');
 $adminMenu = require(__DIR__ . '/adminMenu.php');
+$token = require(__DIR__ . '/token.php');
 
 $config = [
 	'db' => $db,
 	'common' => $common,
     'admin_menu' => $adminMenu,
+    'token' => $token,
 ];
 
 return $config;
---
+++ config/token.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'blogapi' => [
+        'type' => 'Cookie', //类型更换后，tokenCookieName需要变更，避免旧数据串扰
+        'tokenCookieAuthKey' => 'mysweet95', //密钥，type=Cookie方式专用
+        'tokenExpire' => 3600, //服务端过期时间，Cookie方式时，此时间不宜设置太长
+        'tokenSaveInCookie' => 1, //是否把token存入cookie，不保存则需要客户端保存token
+        'tokenCookieName' => 'slp_ss', //保存token的cookie key，不使用也需配置
+        'tokenCookieExpire' => 86400, //0 浏览器退出则失效
+        'tokenCookieSecure' => false, //false Cookie http明文模式，true https加密模式
+        'tokenCookieHttpOnly' => false, //false Web JS可以访问Cookie，true JS禁止读取cookie
+    ],
+];
