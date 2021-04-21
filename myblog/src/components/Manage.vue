a7601463ca4c98450f785f6f98d9cf00b56c3699
flamez57 <flamez57@mysweet95.com>
2021-04-21 16:55:15 +0800

    除头像外修改用户信息ok

--- myblog/src/components/Manage.vue
+++ myblog/src/components/Manage.vue
@@ -9,27 +9,27 @@
                     <p class="header-info-desc">欢迎来到我的个人博客</p>
                 </div>
                 <ul class="left-nav" id="ul1">
-                    <li class="left-nav-item left-nav-action">
+                    <li :class="'/Manage' === path ? 'left-nav-item left-nav-action' : 'left-nav-item'" @click="selectNav('/Manage')">
                         <router-link to="/Manage" class="left-nav-inner">
                             <i class="iconfont icontimerauto" aria-hidden="true"></i>修改个人信息
                         </router-link>
                     </li>
-                    <li class="left-nav-item">
+                    <li :class="'/Manage/SetPwd' === path ? 'left-nav-item left-nav-action' : 'left-nav-item'" @click="selectNav('/Manage/SetPwd')">
                         <router-link to="/Manage/SetPwd" class="left-nav-inner">
                             <i class="iconfont icon-lock" aria-hidden="true"></i>修改密码
                         </router-link>
                     </li>
-                    <li class="left-nav-item">
+                    <li :class="'/Manage/ArticleList' === path ? 'left-nav-item left-nav-action' : 'left-nav-item'" @click="selectNav('/Manage/ArticleList')">
                         <router-link to="/Manage/ArticleList" class="left-nav-inner">
                             <i class="iconfont iconarticle-line" aria-hidden="true"></i>文章
                         </router-link>
                     </li>
-                    <li class="left-nav-item">
+                    <li :class="'/Manage/ManageCate' === path ? 'left-nav-item left-nav-action' : 'left-nav-item'" @click="selectNav('/Manage/ManageCate')">
                         <router-link to="/Manage/ManageCate" class="left-nav-inner">
                             <i class="iconfont iconfolder" aria-hidden="true"></i>分类
                         </router-link>
                     </li>
-                    <li class="left-nav-item">
+                    <li :class="'/Manage/Set' === path ? 'left-nav-item left-nav-action' : 'left-nav-item'" @click="selectNav('/Manage/Set')">
                         <a href="#" class="left-nav-inner">
                             <i class="iconfont iconsettings" aria-hidden="true"></i>设置
                         </a>
@@ -50,10 +50,17 @@ export default {
   name: 'Manage',
   data () {
     return {
-      msg: 'manage'
+      msg: 'manage',
+      path: '/Manage'
     }
   },
+  mounted () {
+    this.path = this.$router.currentRoute.fullPath
+  },
   methods: {
+    selectNav (path) { // 接受路由地址选中当前菜单
+      this.path = path
+    },
     loginOut () {
       this.$api.member.toLoginOut().then(res => {
         // 执行某些操作
