283fd4e03be5a4e54373b658f3a8a74acb7379c3
flamez57 <flamez57@mysweet95.com>
2021-04-23 15:51:09 +0800

    图片上传ok

--- myblog/src/api/base.js
+++ myblog/src/api/base.js
@@ -4,12 +4,11 @@
 
 import Apis from '../config/apis'
 import axios from './http' // 导入http中创建的axios实例
-import qs from 'qs' // 根据需求是否导入qs模块
 
 const base = {
   // 图片上传
   uploadImg (params) {
-    return axios.post(Apis.uploadImg, qs.stringify(params))
+    return axios.post(Apis.uploadImg, params)
   }
 }
 
